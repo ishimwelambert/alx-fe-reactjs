539ef7827572f7bac2fefe43bd05f3587725bdbe
ishimwelambert <168457220+ishimwelambert@users.noreply.github.com>
2024-11-10 10:43:04 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -27,7 +33,21 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+<<<<<<< HEAD
       </p>
+=======
+      </p> */}
+>>>>>>> 8588879b5a44206ff6cdfaa5847426ff5c73da3c
+
+      <WelcomeMessage />
+
+      <Header />
+      
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+      <Footer />
+      
     </>
   )
 }
