6a9decb439e93f54a0a132273c594a945c6fd554
Ishimwe Lambert <ishimwelambert87@gmail.com>
2024-11-20 21:26:47 +0200

    new props

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,8 @@
+// src/UserContext.js
+import React, { createContext } from 'react';
+
+// Initialize the UserContext
+const UserContext = createContext();
+
+// Export the UserContext
+export default UserContext;
