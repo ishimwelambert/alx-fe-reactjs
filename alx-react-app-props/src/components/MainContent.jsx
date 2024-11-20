fb7213056ec345c3dd4f0719eea01318f40016e7
Ishimwe Lambert <ishimwelambert87@gmail.com>
2024-11-20 21:07:47 +0200

    this is react

---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ backgroundColor: "white" , color: '#242424', textAlign: 'center' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+  }
+  
+  export default MainContent;
