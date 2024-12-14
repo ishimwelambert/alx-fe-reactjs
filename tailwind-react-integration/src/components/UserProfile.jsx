186a417f24768a903eb914998017b76855af4e90
Ishimwe Lambert <ishimwelambert87@gmail.com>
2024-12-14 13:41:00 +0200

    changes to userprofile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,15 @@
 const UserProfile = (props) => {
   return (
     <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      {/* User Avatar */}
+      <div className="flex justify-center mb-4">
+        <img 
+          src={props.avatar} 
+          alt={`${props.name}'s avatar`} 
+          className="rounded-full w-36 h-36"
+        />
+      </div>
+      {/* User Details */}
       <h2 className="text-blue-600 text-xl font-semibold">{props.name}</h2>
       <p className="mt-2">
         Age: <span className="font-bold">{props.age}</span>
@@ -11,3 +20,4 @@ const UserProfile = (props) => {
 };
 
 export default UserProfile;
+
